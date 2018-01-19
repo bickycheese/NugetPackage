42f12a309e418941a23585e5a16633c1dee7e0f9
Stanny Nuytkens <nuytkens.stanny@gmail.com>
2018-01-19 11:23:08 +0100

    Add project files.

---
+++ My.Package/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Package
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+#if !DEBUG
+        public int Age { get; set; }
+#endif
+    }
+}
